d0565cb45ecac9feb7d89a6ac74ec3dda801124b
skfoql55-design <skfoql55@gmail.com>
2026-02-27 23:26:14 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,6 +6,7 @@
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey:            "AIzaSyBXuTv3er7JIygCtuznFBLZ5AU1jDfEsTc",
@@ -18,3 +19,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
